7566146db158e7661e50d0c97f4d9a90d5f01d76
Artem Orlov <amorlov.sev@gmail.com>
2024-08-13 12:10:50 +0300

    Добавьте файлы проекта.

---
+++ Chapter01/01_HelloCS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_HelloCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C#!");
+            int n = 2;
+        }
+    }
+}

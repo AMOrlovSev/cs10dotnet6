6c2365350a3f8b9920f458dbf87f36368b50bc75
Artem Orlov <amorlov.sev@gmail.com>
2024-08-25 13:38:50 +0300

    Chapter12

---
+++ Chapter12/06_SynchronizingResourceAccess/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics; // Stopwatch
+using static System.Console;
+
+
+WriteLine("Please wait for the tasks to complete.");
+Stopwatch watch = Stopwatch.StartNew();
+
+Task a = Task.Factory.StartNew(MethodA);
+Task b = Task.Factory.StartNew(MethodB);
+
+Task.WaitAll(new Task[] { a, b });
+
+WriteLine();
+WriteLine($"Results: {SharedObjects.Message}.");
+WriteLine($"{watch.ElapsedMilliseconds:N0} elapsed milliseconds.");
+WriteLine($"{SharedObjects.Counter} string modifications.");
+
+
+//static void MethodA()
+//{
+//    lock (SharedObjects.Conch)
+//    {
+//        for (int i = 0; i < 5; i++)
+//        {
+//            Thread.Sleep(SharedObjects.Random.Next(2000));
+//            SharedObjects.Message += "A";
+//            Write(".");
+//        }
+//    }
+//}
+
+static void MethodA()
+{
+    try
+    {
+        if (Monitor.TryEnter(SharedObjects.Conch, TimeSpan.FromSeconds(15)))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(SharedObjects.Random.Next(2000));
+                SharedObjects.Message += "A";
+                Interlocked.Increment(ref SharedObjects.Counter);
+                Write(".");
+            }
+        }
+        else
+        {
+            WriteLine("Method A timed out when entering a monitor on conch.");
+        }
+    }
+    finally
+    {
+        Monitor.Exit(SharedObjects.Conch);
+    }
+}
+
+//static void MethodB()
+//{
+//    lock (SharedObjects.Conch)
+//    {
+//        for (int i = 0; i < 5; i++)
+//        {
+//            Thread.Sleep(SharedObjects.Random.Next(2000));
+//            SharedObjects.Message += "B";
+//            Write(".");
+//        }
+//    }
+//}
+
+static void MethodB()
+{
+    try
+    {
+        if (Monitor.TryEnter(SharedObjects.Conch, TimeSpan.FromSeconds(15)))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(SharedObjects.Random.Next(2000));
+                SharedObjects.Message += "B";
+                Interlocked.Increment(ref SharedObjects.Counter);
+                Write(".");
+            }
+        }
+        else
+        {
+            WriteLine("Method B timed out when entering a monitor on conch.");
+        }
+    }
+    finally
+    {
+        Monitor.Exit(SharedObjects.Conch);
+    }
+}
+
+static class SharedObjects
+{
+    public static object Conch = new();
+    public static Random Random = new();
+    public static string? Message; // общий ресурс
+    public static int Counter; // иной общий ресурс
+}
